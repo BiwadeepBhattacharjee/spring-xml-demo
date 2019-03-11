4a0c54b4b0aa1b28144ef7248a1fd60435bb2f41
biswadeep <biswadeep87@gmail.com>
2019-03-11 18:21:31 +0530

    second

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -11,6 +11,10 @@ public class Movie {
         this.actor = actor;
     }
 
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
